603707423ad04f0135bba039833d71fa22f6cfe0
Luca Bennati <102149845+asemlucben@users.noreply.github.com>
2023-09-12 16:28:10 +0200

    Delete ProjectFiles directory

--- ProjectFiles/coolfireworks/script.js
+++
@@ -1,9 +0,0 @@
-async function loadParticles(options) {
-  await loadFireworksPreset(tsParticles);
-
-  await tsParticles.load(options);
-}
-
-const configs = { preset: "fireworks" };
-
-loadParticles(configs);
